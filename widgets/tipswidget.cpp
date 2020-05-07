619ba29a258a215868a05e83f543f8a030f08d1d
zhaolong <zhaolong@deepin.com>
2020-05-07 16:28:16 +0800

    fix(bluetooth):popups display truncated
    task20793 【TR4】【桌面专业版】【SP1】【华为】【Kunpeng920】【uos-20-pangu-daliy-20200424-build46】【任务栏】任务栏蓝牙显示截断

--- widgets/tipswidget.cpp
+++ widgets/tipswidget.cpp
@@ -12,6 +12,7 @@ TipsWidget::TipsWidget(QWidget *parent) : QFrame(parent)
 
 void TipsWidget::setText(const QString &text)
 {
+    m_type = TipsWidget::SingleLine;
     m_text = text;
 
     setFixedSize(fontMetrics().width(text) + 6, fontMetrics().height());
@@ -21,6 +22,7 @@ void TipsWidget::setText(const QString &text)
 
 void TipsWidget::setTextList(const QStringList &textList)
 {
+    m_type = TipsWidget::MultiLine;
     m_textList = textList;
 
     int maxLength = 0;
@@ -45,12 +47,13 @@ void TipsWidget::paintEvent(QPaintEvent *event)
     int fontHeight = fontMetrics().height();
     option.setAlignment(Qt::AlignCenter);
 
-    if (!m_text.isEmpty() && m_textList.isEmpty()) {
+    switch (m_type) {
+    case SingleLine: {
         painter.drawText(rect(), m_text, option);
     }
-
-    int y = 0;
-    if (m_text.isEmpty() && !m_textList.isEmpty()) {
+        break;
+    case MultiLine: {
+        int y = 0;
         if (m_textList.size() != 1)
             option.setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
         for (QString text : m_textList) {
@@ -58,5 +61,6 @@ void TipsWidget::paintEvent(QPaintEvent *event)
             y += fontHeight;
         }
     }
-
+        break;
+    }
 }
